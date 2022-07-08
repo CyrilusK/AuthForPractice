6b8d4aebdb46b95eaf3c0f4d0f1bd28bf2150d2a
CyrilusK <kardash199696@mail.ru>
2022-07-08 23:55:34 +0300

    added interface

--- Auth-Firebase/Auth-Firebase/ViewController.swift
+++ Auth-Firebase/Auth-Firebase/ViewController.swift
@@ -1,19 +1,22 @@
-//
-//  ViewController.swift
-//  Auth-Firebase
-//
-//  Created by Кирилл Кардаш on 08.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var passwordField: UITextField!
+    let button = UIButton(type: .custom)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.setImage(UIImage(systemName: "eye"), for: .normal)
+        //button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
+        button.frame = CGRect(x: CGFloat(passwordField.frame.size.width - 25), y: CGFloat(10), width: CGFloat(25), height: CGFloat(25))
+        button.addTarget(self, action: #selector(self.refresh), for: .touchUpInside)
+        passwordField.rightView = button
+        passwordField.rightViewMode = .always
     }
 
+    @IBAction func refresh(_ sender: Any) {
+        print("ok")
+    }
 
 }
 
