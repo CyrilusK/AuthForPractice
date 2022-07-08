cc9bbacd659d4c03f38e8298a8339540f55acb66
CyrilusK <kardash199696@mail.ru>
2022-07-08 20:21:02 +0300

    created new project

---
+++ Auth-Firebase/Auth-Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auth-Firebase
+//
+//  Created by Кирилл Кардаш on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
