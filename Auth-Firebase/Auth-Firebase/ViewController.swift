eead361b2a730df56f8bef8c791883cf6e9296f8
CyrilusK <kardash199696@mail.ru>
2022-07-09 04:09:21 +0300

    added icons

--- Auth-Firebase/Auth-Firebase/ViewController.swift
+++ Auth-Firebase/Auth-Firebase/ViewController.swift
@@ -1,21 +1,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var passwordField: UITextField!
-    let button = UIButton(type: .custom)
+    @IBOutlet weak var loginField: UITextField!
+    
+    var btnEye = UIButton(type: .custom)
+    var btnMail = UIButton(type: .custom)
+    var btnLock = UIButton(type: .custom)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.setImage(UIImage(systemName: "eye"), for: .normal)
-        //button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
-        button.frame = CGRect(x: CGFloat(passwordField.frame.size.width - 25), y: CGFloat(10), width: CGFloat(25), height: CGFloat(25))
-        button.addTarget(self, action: #selector(self.refresh), for: .touchUpInside)
-        passwordField.rightView = button
+        
+        btnEye.setImage(UIImage(systemName: "eye"), for: .normal)
+        btnEye.tintColor = UIColor(named: "colorForBtns")
+        btnEye.addTarget(self, action: #selector(self.refresh), for: .touchUpInside)
+        passwordField.rightView = btnEye
         passwordField.rightViewMode = .always
+        
+        btnLock.setImage(UIImage(systemName: "lock"), for: .normal)
+        btnLock.tintColor = UIColor(named: "colorForBtns")
+        passwordField.leftView = btnLock
+        passwordField.leftViewMode = .always
+        
+        btnMail.setImage(UIImage(systemName: "envelope.badge.shield.half.filled"), for: .normal)
+        btnMail.tintColor = UIColor(named: "colorForBtns")
+        loginField.leftView = btnMail
+        loginField.leftViewMode = .always
     }
 
-    @IBAction func refresh(_ sender: Any) {
-        print("ok")
+    @IBAction func refresh(_ sender: UIButton) {
+        if passwordField.isSecureTextEntry {
+            btnEye.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+            passwordField.isSecureTextEntry = false
+        } else {
+            btnEye.setImage(UIImage(systemName: "eye"), for: .normal)
+            passwordField.isSecureTextEntry = true
+        }
     }
 
 }
